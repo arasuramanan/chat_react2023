58a1cc6ceab76755e6adc936bec06aab0d3c7fad
arasuramanan <arasuaudi@gmail.com>
2023-06-28 14:26:47 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import Typography from "@mui/material/Typography";
+
+export default function Home() {
+  return (
+    <>
+      <Typography>
+        <center><h1>Welcome to my Chat App</h1></center>
+        <center><h3>Using Method of this chat app</h3></center>
+        <p>In the navbar have two users created and stored in the db, one tab click one user and another tab click another user to use this app.</p>
+        <p>You want create a new user or new chat room, Click login button in the home page.</p>
+        <p>You will create a new user also, the defalut user name is test.</p>
+        <p>You want better ui experience open two different browser in side by side view.</p>       
+      </Typography>
+    </>
+  );
+}
